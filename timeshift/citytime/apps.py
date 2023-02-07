d05ece0888046a9c0803875682fa0957e478f4e2
MikhailDevW <wildmv@gmail.com>
2023-02-07 15:16:06 +0300

    URLs and views added. First page done.

---
+++ timeshift/citytime/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CitytimeConfig(AppConfig):
+    name = 'citytime'
---
+++ timeshift/citytime/urls.py
@@ -0,0 +1,10 @@
+# ice_cream/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Главная страница
+    path('', views.index),
+    # Страница со списком сортов мороженого
+    # path('ice_cream/', views.ice_cream_list),
+]
