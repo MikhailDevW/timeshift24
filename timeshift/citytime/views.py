bcc369775b54a9a730b89ab1a22c66293da390f3
MikhailDevW <wildmv@gmail.com>
2023-02-07 16:51:22 +0300

    fonts added

--- timeshift/citytime/views.py
+++ timeshift/citytime/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import os
+
 # Create your views here.
 # Главная страница
 def index(request):    
-    return HttpResponse('Главная страница')
+    template = 'index.html'
+    
+    
+    print('>>>', os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    context = {
+        # В словарь можно передать переменную
+        'city': 'gdfdgfg',
+        # А можно сразу записать значение в словарь. Но обычно так не делают
+        'date': '6',
+    }
+    return render(request, template, context)
--- timeshift/timeshift/settings.py
+++ timeshift/timeshift/settings.py
@@ -52,10 +52,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'timeshift.urls'
 
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),
+                    os.path.join(BASE_DIR, 'templates/css/'),
+                    os.path.join(BASE_DIR, 'templates/fonts/'),)
