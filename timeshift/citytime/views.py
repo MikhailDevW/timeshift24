d457eca1906ea362efbc544dc6420f20a9f22db0
MikhailDevW <wildmv@gmail.com>
2023-02-11 16:16:38 +0300

    environment fixes and improvement

--- timeshift/citytime/views.py
+++ timeshift/citytime/views.py
@@ -7,10 +7,24 @@ def index(request):
     template = 'index.html'
     response_data = dict()
     data = dict()
+    month: dict = {
+        '01': 'января',
+        '02': 'февраля',
+        '03': 'марта',
+        '04': 'апреля',
+        '05': 'мая',
+        '06': 'июня',
+        '07': 'июля',
+        '08': 'августа',
+        '09': 'сентября',
+        '10': 'октября',
+        '11': 'ноября',
+        '12': 'декабря',
+    }
 
     # Если поступил POST запрос
     if request.method == 'POST':
-        city_name = request.POST['city_name']
+        city_name = request.POST['city_name'].lower().capitalize()
         api = ts.AbstractAPI()
         response_data = api.fetch_city_data(city_name)
 
@@ -18,7 +32,12 @@ def index(request):
         if not response_data.get('error', False):
             data = {
                 'name': city_name,
-                'local_time': response_data['datetime'][-8:],
+                'hours': response_data['datetime'][11: 13],
+                'minutes': response_data['datetime'][14: 16],
+                'seconds': response_data['datetime'][17:],
+                'date': response_data['datetime'][8:10],
+                'month': month[response_data['datetime'][5:7]],
+                'year': response_data['datetime'][:4],
             }
             return render(request, template, data)
         # в случае если город не найден через апи
@@ -33,6 +52,11 @@ def index(request):
         response_data = api.fetch_city_data('Moscow')
         data = {
             'name': 'Москва',
-            'local_time': response_data['datetime'][-8:],
+            'hours': response_data['datetime'][11: 13],
+            'minutes': response_data['datetime'][14: 16],
+            'seconds': response_data['datetime'][17:],
+            'date': response_data['datetime'][8:10],
+            'month': month[response_data['datetime'][5:7]],
+            'year': response_data['datetime'][:4],
         }
         return render(request, template, data)
